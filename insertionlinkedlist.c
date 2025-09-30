1e7312826f630ccceffd4d372322df910c1ff707
amaljoebiju <amaljoebiju2029@mca.ajce.in>
2025-09-30 10:18:59 +0530

    Update insertionlinkedlist.c

--- insertionlinkedlist.c
+++ insertionlinkedlist.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct Node {
     int data;
     struct Node* next;
 };
-
 
 struct Node* createNode(int value) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -28,6 +26,10 @@ void insertEnd(struct Node** head, int value) {
     temp->next = newNode;
 }
 
+// ✅ New function to add roll no 14
+void addRoll14(struct Node** head) {
+    insertEnd(head, 14);
+}
 
 void displayList(struct Node* head) {
     printf("Roll Numbers in Linked List: ");
@@ -45,19 +47,15 @@ void displayList(struct Node* head) {
 int main() {
     struct Node* head = NULL;
 
-
     insertEnd(&head, 42);
 
-
-    insertEnd(&head, 14);
+    // using new function to add roll number 14
+    addRoll 14(&head);
+    insertEnd(&head,  7);
     insertEnd(&head, 13);
     insertEnd(&head, 30);
-
 
     displayList(head);
 
     return 0;
 }
-
-
-
