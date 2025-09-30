f09fc6ddb25599caaa722e3d000f099590873391
amaljoebiju <amaljoebiju2029@mca.ajce.in>
2025-09-30 10:23:26 +0530

    Update insertionlinkedlist.c

--- insertionlinkedlist.c
+++ insertionlinkedlist.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Node structure
 struct Node {
     int data;
     struct Node* next;
 };
 
-
+// Function to create a new node
 struct Node* createNode(int value) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
     newNode->data = value;
     newNode->next = NULL;
     return newNode;
 }
 
+// Insert a new node at the end
 void insertEnd(struct Node** head, int value) {
     struct Node* newNode = createNode(value);
     if (*head == NULL) {
@@ -28,7 +33,12 @@ void insertEnd(struct Node** head, int value) {
     temp->next = newNode;
 }
 
+// Function to specifically add roll number 14
+void addRoll14(struct Node** head) {
+    insertEnd(head, 14);
+}
 
+// Display the linked list
 void displayList(struct Node* head) {
     printf("Roll Numbers in Linked List: ");
     struct Node* temp = head;
@@ -42,22 +52,18 @@ void displayList(struct Node* head) {
     printf("\n");
 }
 
+// Main function
 int main() {
     struct Node* head = NULL;
 
+    // Insert values into the linked list
+    insertEnd(&head, 42);   // Insert 42
+    addRoll14(&head);       // Insert 14 using new function
+    insertEnd(&head, 13);   // Insert 13
+    insertEnd(&head, 30);   // Insert 30
 
-    insertEnd(&head, 42);
-
-
-    insertEnd(&head, 14);
-    insertEnd(&head, 13);
-    insertEnd(&head, 30);
-
-
+    // Display the list
     displayList(head);
 
     return 0;
 }
-
-
-
