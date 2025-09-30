59084b4f6eea915bf6ece153cb8948d0b0a2b761
amaljoebiju <amaljoebiju2029@mca.ajce.in>
2025-09-30 10:37:40 +0530

    Update insertionlinkedlist.c

--- insertionlinkedlist.c
+++ insertionlinkedlist.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct Node {
     int data;
     struct Node* next;
 };
-
 
 struct Node* createNode(int value) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -28,7 +26,6 @@ void insertEnd(struct Node** head, int value) {
     temp->next = newNode;
 }
 
-
 void displayList(struct Node* head) {
     printf("Roll Numbers in Linked List: ");
     struct Node* temp = head;
@@ -42,22 +39,33 @@ void displayList(struct Node* head) {
     printf("\n");
 }
 
+
+void searchNode(struct Node* head, int value) {
+    struct Node* temp = head;
+    int position = 1; 
+    while (temp != NULL) {
+        if (temp->data == value) {
+            printf("Value %d found at position %d in the list.\n", value, position);
+            return;
+        }
+        temp = temp->next;
+        position++;
+    }
+    printf("Value %d not found in the list.\n", value);
+}
+
 int main() {
     struct Node* head = NULL;
 
-
     insertEnd(&head, 42);
-
-
     insertEnd(&head, 14);
     insertEnd(&head, 13);
     insertEnd(&head, 30);
 
-
     displayList(head);
+
+   
+    searchNode(head, 14);
 
     return 0;
 }
-
-
-
